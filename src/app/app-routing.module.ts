d5f8c05c44d048a327f88cd1704f1acdadee0b13
adem279 <SAHBANI.Adem@esprit.tn>
2026-02-13 18:56:33 +0100

    Partie1Workshop3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Route par défaut - redirige vers /home
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  
+  // Route vers la page d'accueil
+  { path: 'home', component: HomeComponent },
+  
+  // Route vers la liste des suggestions
+  { path: 'listSuggestion', component: ListSuggestionComponent },
+  
+  // Route 404 - doit être en dernier
+  { path: '**', component: NotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
